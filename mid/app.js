040226169764056672439491fcd5139f06655578
koresmosto <koresmosto@gmail.com>
2021-11-27 22:38:17 +0200

    html/css basics

--- mid/app.js
+++ mid/app.js
@@ -7,16 +7,21 @@ const connectLivereload = require("connect-livereload");
 const port = 3000
 
 const liveReloadServer = livereload.createServer();
-liveReloadServer.watch(path.join(__dirname, '.'));
+liveReloadServer.watch(path.join(__dirname, 'public'));
 
 app.use(connectLivereload());
+app.use(express.static('public'));
 
 app.get('/js', (req, res) => {
     res.send('Hello World!')
 })
 
 app.get(['/', '/index', '/index.html'], function (req, res) {
-    res.sendFile(path.join(__dirname, '/index.html'));
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get(['/vm', '/vm.html', '/vm.htm'], function (req, res) {
+    res.sendFile(path.join(__dirname, '/public/vm.html'));
 });
 
 app.listen(port, () => {
