5e4ea4f52d3238318be04df951bda8fcb4ae3df2
stingion <stingion@gmail.com>
2022-02-09 17:09:00 +0200

    #5 Play with reactjs: add modal

--- react-temp/src/App.js
+++ react-temp/src/App.js
@@ -1,14 +1,28 @@
-import React from "react"
+import React, {useEffect} from "react"
 import TodoList from "./todo/TodoList"
-import AddTodo from "./todo/AddTodo"
-import Context from "./context";
+import Context from "./context"
+import Loader from "./todo/Loader"
+import Modal from './modal/Modal'
+// import AddTodo from './todo/AddTodo'
+
+const AddTodo = React.lazy(() => new Promise(resolve => {
+    setTimeout(() => resolve(import('./todo/AddTodo')), 2500)
+}))
 
 function App() {
-    const [todos, setTodos] = React.useState([
-        {id: 1, completed: false, title: 'Buy bread'},
-        {id: 2, completed: true, title: 'Buy juice'},
-        {id: 3, completed: false, title: 'Buy oil'}
-    ])
+    const [todos, setTodos] = React.useState([])
+    const [loading, setLoading] = React.useState(true)
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+            .then(response => response.json())
+            .then(todos => {
+                setTimeout(() => {
+                    setTodos(todos)
+                    setLoading(false)
+                }, 1500)
+            })
+    }, [])
 
     function toggleTodo(id) {
         setTodos(todos.map(
@@ -38,11 +52,21 @@ function App() {
         <Context.Provider value={{removeTodo}}>
             <div className='wrapper'>
                 <h1>React todos</h1>
-                <AddTodo onCreate={addTodo}/>
+
+                <Modal/>
+
+                <React.Suspense fallback={<Loader/>}>
+                    <AddTodo onCreate={addTodo}/>
+                </React.Suspense>
+
                 <hr/>
+
+                {loading && <Loader/>}
                 {todos.length
                     ? <TodoList todos={todos} onToggle={toggleTodo}/>
-                    : <p>no todos</p>
+                    : (
+                        loading ? null : <p>no todos</p>
+                    )
                 }
             </div>
         </Context.Provider>
